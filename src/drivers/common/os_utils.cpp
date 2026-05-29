4278dff55ea4a3775fcb0bb38cb509cd9e4f7cd6
Laffinty <lafinty787@gmail.com>
2026-05-30 07:24:19 +0800

    [irreversible] remove C++ fallback for os_utils

--- src/drivers/common/os_utils.cpp
+++ src/drivers/common/os_utils.cpp
@@ -3,8 +3,6 @@
 #include <stdlib.h>
 
 #include "common/os_utils.h"
-
-#ifdef FCEUX11_RUST_ENABLED
 #include "../../rust/fceux11_rust.h"
 
 int fceu_mkdir(const char *path)
@@ -26,73 +24,3 @@ int msleep(int ms)
 {
 	return fceux11_rust_msleep(ms);
 }
-
-#else // !FCEUX11_RUST_ENABLED — original C++ implementation
-
-#ifndef WIN32
-#error "Platform not supported"
-#endif
-
-#include <windows.h>
-#include <direct.h>
-#include <io.h>
-
-int fceu_mkdir(const char *path)
-{
-	int retval;
-	retval = _mkdir(path);
-	_chmod(path, 755);
-	return retval;
-}
-
-int fceu_mkpath(const char *path)
-{
-	int i, retval = 0;
-	char p[512];
-
-	i = 0;
-	while (path[i] != 0)
-	{
-		if (path[i] == '/')
-		{
-			if (i > 0)
-			{
-				p[i] = 0;
-
-				retval = fceu_mkdir(p);
-
-				if (retval)
-				{
-					return retval;
-				}
-			}
-		}
-		p[i] = path[i]; i++;
-	}
-	p[i] = 0;
-
-	retval = fceu_mkdir(p);
-
-	return retval;
-}
-
-bool fceu_file_exists(const char *filepath)
-{
-	FILE *fp;
-	fp = ::fopen(filepath, "r");
-
-	if (fp != NULL)
-	{
-		::fclose(fp);
-		return true;
-	}
-	return false;
-}
-
-int msleep(int ms)
-{
-	Sleep(ms);
-	return 0;
-}
-
-#endif // !FCEUX11_RUST_ENABLED
